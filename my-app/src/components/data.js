c39c15ba260b45cf8fcf853e39f31df918122ef3
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-05-03 00:10:17 +0400

    hello

---
+++ my-app/src/components/data.js
@@ -0,0 +1,27 @@
+export const students = [
+    {
+        id:1,
+        name:'Nurlana',
+        age:23
+    },
+    {
+        id:2,
+        name:'Laman',
+        age:21
+    },
+    {
+        id:3,
+        name:'Aysel',
+        age:22
+    },
+    {
+        id:4,
+        name:'Ayshan',
+        age:14
+    },
+    {
+        id:5,
+        name:'Yazgul',
+        age:44
+    }
+]
